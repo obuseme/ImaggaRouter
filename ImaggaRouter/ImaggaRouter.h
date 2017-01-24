7b01768f14d35b23f5cc0ad1c37551d80cc8636d
Andy Obusek <andyo@aweber.com>
2017-01-23 20:23:45 -0500

    Initial router

---
+++ ImaggaRouter/ImaggaRouter.h
@@ -0,0 +1,19 @@
+//
+//  ImaggaRouter.h
+//  ImaggaRouter
+//
+//  Created by Andy Obusek on 1/23/17.
+//  Copyright © 2017 raywenderlich.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImaggaRouter.
+FOUNDATION_EXPORT double ImaggaRouterVersionNumber;
+
+//! Project version string for ImaggaRouter.
+FOUNDATION_EXPORT const unsigned char ImaggaRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImaggaRouter/PublicHeader.h>
+
+
